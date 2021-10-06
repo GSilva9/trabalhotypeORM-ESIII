86712d4e3ab5a716057cca7942bab9d21eda23d8
Giovane Silva <giovane.gomes94@hotmail.com>
2021-10-05 23:01:20 -0300

    :sparkles: Add table establishment

--- src/migration/1633474490618-createEstablishment.ts
+++ src/migration/1633474490618-createEstablishment.ts
@@ -1,11 +1,50 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class createEstablishment1633474490618 implements MigrationInterface {
-
+export class CreateEstablishment1633474490618 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'Estabelishment',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uiid_generate_v4()'
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'doc',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'site',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'contributors',
+                        type: 'integer',
+                    },
+                    {
+                        name: 'sumOfProducts',
+                        type: 'integer',
+                    },
+                    {
+                        name: 'update_At',
+                        type: 'timestamp',
+                        default: 'now()',
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.dropTable('Estabelishment')
     }
 
 }
